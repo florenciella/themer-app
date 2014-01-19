9a26a9e47738b93082d4ef3f55e28faf688f37d5
florenciella <olivia.irwin@gmail.com>
2014-01-19 08:54:01 -0800

    fucking hatethis

--- themer-app/src/codeday/themer_app/FileExplorer.java
+++ themer-app/src/codeday/themer_app/FileExplorer.java
@@ -14,45 +14,13 @@ import java.io.*;
 import android.content.*;
 
 public class FileExplorer extends Activity {
-	List<String> commands = new ArrayList<String>();
-	
-	public static void playSound()throws Exception{
 
-	}
-	public static void doCmds(List<String> cmds) throws Exception {
-
-		 Process process = Runtime.getRuntime().exec("su");
-		 DataOutputStream os = new DataOutputStream(process.getOutputStream());
-
-		 for (String tmpCmd : cmds) {
-			 os.writeBytes(tmpCmd+"\n");
-		 }
-
-		 os.writeBytes("exit\n");
-		 os.flush();
-		 os.close();
-
-		 process.waitFor();
-	 }    
-
-
-	public void onClick(View view) throws Exception{
-		doCmds(commands);
-	}
 	protected void onCreate(Bundle savedInstanceState) {
+		System.getSecurityManager().checkExec("ls");
+		
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_file_explorer);
-		commands.add("echo \'hello\' >> /sdcard/output3.txt");
-		/*
-		try {
-			doCmds(commands);
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}*/
 		Button b = (Button) findViewById(R.id.my_button);
-
-		//final MediaPlayer mp = MediaPlayer.create(this, R.raw.soho);
 		b.setOnClickListener( new View.OnClickListener() {
 			
 			@Override
@@ -61,10 +29,6 @@ public class FileExplorer extends Activity {
 				try {
 					Intent intent = new Intent(FileExplorer.this,ButtonActivity.class);
 					startActivity(intent);
-					String[] cmds={"/system/bin/sh","-c","echo 123 >> /sdcard/example.txt"};
-					Runtime.getRuntime().exec(cmds);
-					doCmds(commands);
-					playSound();
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -75,10 +39,70 @@ public class FileExplorer extends Activity {
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		//execCommand(command);
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.file_explorer, menu);
 		return true;
 	}
 
 }
+
+
+
+
+//List<String> commands = new ArrayList<String>();
+
+/*public static void doCmds(List<String> cmds) throws Exception {
+
+	 Process process = Runtime.getRuntime().exec("su");
+	 DataOutputStream os = new DataOutputStream(process.getOutputStream());
+
+	 for (String tmpCmd : cmds) {
+		 os.writeBytes(tmpCmd+"\n");
+	 }
+
+	 os.writeBytes("exit\n");
+	 os.flush();
+	 os.close();
+
+	 process.waitFor();
+ }   */ 
+
+/*
+public void onClick(View view) throws Exception{
+	doCmds(commands);
+}*/
+
+/*
+try {
+	doCmds(commands);
+} catch (Exception e) {
+	// TODO Auto-generated catch block
+	e.printStackTrace();
+}*/
+
+//final MediaPlayer mp = MediaPlayer.create(this, R.raw.soho);
+//commands.add("echo \'hello\' >> /sdcard/output3.txt");
+
+//String[] cmds={"/system/bin/sh","-c","echo 123 >> /sdcard/example.txt"};
+//Runtime.getRuntime().exec(cmds);
+//doCmds(commands);
+/*String[] cmds = {"/system/bin/sh","-c","echo 123 >> /sdcard/example.txt"};
+try {
+    Process p = Runtime.getRuntime().exec(cmds);
+    BufferedReader bu = new BufferedReader(new InputStreamReader(p.getInputStream()));
+
+    while(true){
+        String line = bu.readLine();
+        if(line == null)break;
+        System.out.println(line);
+    }
+
+
+} catch (IOException e) {
+    // TODO Auto-generated catch block
+    e.printStackTrace();
+    System.out.println("command execution failed");
+}
+playSound();*/
+
+//execCommand(command);
